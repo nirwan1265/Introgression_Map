9fa9c27b0c8182f74bcb4da8710b4b4eeb5c3484
nirwan1265 <tandukar.nirwan@gmail.com>
2023-12-02 15:51:25 -0500

    update

--- src/3.run_Intro_Mapping.R
+++ src/3.run_Intro_Mapping.R
@@ -24,11 +24,11 @@ output <- paste0(getwd(),"/result")
 myres2 = RTIGER(expDesign = expDesign,
                outputdir = output,
                seqlengths = chr_len,
-               rigidity = 700,
+               rigidity = 500,
                autotune = FALSE,
                average_coverage = 0.8,
                nstates = 3,
-               crossovers_per_megabase = 1, 
+               crossovers_per_megabase = 100, 
                post.processing = FALSE,
                save.results = TRUE)
 RTIGER::calcCOnumber(myres)
--- src/4.calculate_heterozygous.R
+++ src/4.calculate_heterozygous.R
@@ -1,23 +1,69 @@
 install.packages("rCNV")
 library(rCNV)
+library(vcfR)
 
-
-vcf <- readVCF("~/Desktop/genotype_diplo_chr10_hardfilter_bcffiltered.vcf.gz")
+vcf <- readVCF("~/Downloads/genotype_diplo_chr1_hardfilter_bcffiltered_bialleles.vcf.gz")
 
 pp<-substr(colnames(vcf$vcf)[-c(1:9)],1,8)
+hzygots <- h.zygosity(vcf,plot=TRUE,pops=pp)
 
 
 
 
-?maf()
-?hetTgen()
 
-h.table <- hetTgen(
-  vcf,
-  info.type = c("AD", "AD-tot", "GT", "GT-012", "GT-AB", "DP"),
-  verbose = TRUE
-)
+str(hzygots)
 
-h.table <- maf(h.table, AD = TRUE, verbose = TRUE)
-pp<-substr(colnames(h.tabl)[-c(1:9)],1,8)
-hzygots<-h.zygosity(h.table,plot=TRUE,pops=pp)
+hzygots$heterozygotes <- hzygots$total - hzygots$`O(Hom)`
+hzygots$heterozygosity <- hzygots$heterozygotes/hzygots$total
+
+
+vcf <- readVCF("~/Downloads/genotype_diplo_chr1_hardfilter_bcffiltered_bialleles.vcf.gz")
+
+vcf2 <- read.vcfR("~/Downloads/genotype_diplo_chr1_hardfilter_bcffiltered_bialleles.vcf.gz")
+geno <- extract.gt(vcf2, element = "GT")
+
+str(geno)
+
+# Calculate the percentage of NA in each row
+na_percentage <- apply(geno, 1, function(x) {
+  na_count <- sum(is.na(x))
+  total_count <- length(x)
+  return((na_count / total_count) * 100)
+})
+
+# Plot a histogram of the NA percentages
+hist(na_percentage, main = "Histogram of Missing Data Percentage", 
+     xlab = "Percentage of Missing Data", 
+     ylab = "Frequency", 
+     col = "blue", 
+     breaks = 10) # Adjust 'breaks' as needed for bin size
+
+
+# Function to count genotypes
+count_genotypes <- function(column) {
+  homo_ref_count <- sum(column == "0/0", na.rm = TRUE)
+  homo_alt_count <- sum(column == "1/1", na.rm = TRUE)
+  hetero_count <- sum(column == "0/1" | column == "1/0", na.rm = TRUE)
+  return(c(homo_ref = homo_ref_count, homo_alt = homo_alt_count, hetero = hetero_count))
+}
+
+# Apply the function to each sample (column)
+genotype_counts <- apply(geno, 2, count_genotypes)
+
+# Convert to a data frame
+genotype_counts_df <- as.data.frame(t(genotype_counts))
+
+# Naming the rows after the samples
+rownames(genotype_counts_df) <- colnames(geno)
+
+# Display the data frame
+genotype_counts_df
+
+
+# Calculate the percentages and add them to the data frame
+genotype_counts_df$homo_ref_perc <- (genotype_counts_df$homo_ref / rowSums(genotype_counts_df)) * 100
+genotype_counts_df$homo_alt_perc <- (genotype_counts_df$homo_alt / rowSums(genotype_counts_df)) * 100
+genotype_counts_df$hetero_perc <- (genotype_counts_df$hetero / rowSums(genotype_counts_df)) * 100
+
+# Display the updated data frame
+genotype_counts_df
